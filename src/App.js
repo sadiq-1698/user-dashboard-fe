a758d93bc54367914a9b3e80465bb690f210eae8
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-07-13 12:39:05 +0530

    WIP: pie chart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import "./App.scss";
 import Routes from "./routes/Routes";
 
-// app component
 function App() {
   return (
     <div className="App">
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -21,7 +21,28 @@ const Main = () => {
         })}
       </div>
 
+      <TopProductsBox />
+
       <ScheduleBox />
+    </div>
+  );
+};
+
+const TopProductsBox = () => {
+  return (
+    <div className="top-products-box">
+      <div className="top-products-box-head">
+        <h3>Top Products</h3>
+        <div className="right">
+          <p>May - June 2021&nbsp;</p>
+          <div className="chevron-down">&gt;</div>
+        </div>
+      </div>
+
+      <div className="top-products-box-body">
+        <div className="pie-chart"></div>
+        <div className="pie-stats"></div>
+      </div>
     </div>
   );
 };
