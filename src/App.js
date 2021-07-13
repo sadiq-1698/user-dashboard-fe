00bc091a365ac28549a863a44b09626b3148719c
sadiq-1698 <mohammedsadiq1698@gmail.com>
2021-07-13 11:51:09 +0530

    max wrapper styles for app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
 import "./App.scss";
-// import Dashboard from "./pages/Dashboard/Dashboard";
 import Routes from "./routes/Routes";
-// import Register from "./pages/Register/Register";
-// import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
-      {/* <Register /> */}
-      {/* <Dashboard /> */}
       <Routes />
     </div>
   );
